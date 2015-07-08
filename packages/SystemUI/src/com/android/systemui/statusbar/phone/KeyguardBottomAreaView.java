83007f48f18b8762d71080f99e036f7c55990f99
Roman Birg <roman@cyngn.com>
2015-07-07 18:34:12 -0700

    SystemUI: fix keyguard unlock drawable not updating
    REF: NIGHTLIES-1247
    Change-Id: Idd4e3653b6bd4976be8211cca8b8a4484a6334e9
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -541,7 +541,6 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         return false;
     }
 
-    @Override
     public void onUnlockMethodStateChanged() {
         mLockIcon.update();
         updateCameraVisibility();
