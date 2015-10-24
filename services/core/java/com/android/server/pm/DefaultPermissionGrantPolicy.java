e40b215297aaa15cfb275763b83880010ac8b506
xyyx <xyyx@mail.ru>
2015-10-24 12:08:29 +0800

    Browser: Add storage permissions

--- services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
@@ -492,6 +492,7 @@ final class DefaultPermissionGrantPolicy {
             }
             if (browserPackage != null
                     && doesPackageSupportRuntimePermissions(browserPackage)) {
+                grantRuntimePermissionsLPw(browserPackage, STORAGE_PERMISSIONS, userId);
                 grantRuntimePermissionsLPw(browserPackage, LOCATION_PERMISSIONS, userId);
             }
 
