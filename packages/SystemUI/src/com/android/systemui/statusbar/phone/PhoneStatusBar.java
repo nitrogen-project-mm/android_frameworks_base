94fffb6c4c6eb1302bac66906d131a4962ddaf3d
d34d <clark@cyngn.com>
2016-01-15 15:24:14 -0800

    SysUI: Fix keyguard wallpaper not updating
    It helps if we actually act on the ACTION_KEYGUARD_WALLPAPER_CHANGED
    ;)
    
    Change-Id: I96b43d61fcb6d21e02c972b6897dbba34628a187
    TICKET: CYNGNOS-1624

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3079,6 +3079,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
             else if (Intent.ACTION_SCREEN_ON.equals(action)) {
                 notifyNavigationBarScreenOn(true);
+            } else if (Intent.ACTION_KEYGUARD_WALLPAPER_CHANGED.equals(action)) {
+                WallpaperManager wm = (WallpaperManager) mContext.getSystemService(
+                        Context.WALLPAPER_SERVICE);
+                mKeyguardWallpaper = wm.getKeyguardBitmap();
+                updateMediaMetaData(true);
             }
         }
     };
