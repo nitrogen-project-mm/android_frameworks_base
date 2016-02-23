ad2895a978641f2433a0ff5501f15a880cfeeec6
xyyx <xyyx@mail.ru>
2016-02-23 09:12:29 +0800

    Add overlay option for Settings.System.NAVIGATION_BAR_SHOW

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2391,6 +2391,9 @@ class DatabaseHelper extends SQLiteOpenHelper {
             loadIntegerSetting(stmt, Settings.System.POINTER_SPEED,
                     R.integer.def_pointer_speed);
 
+            loadIntegerSetting(stmt, Settings.System.NAVIGATION_BAR_SHOW,
+                    R.integer.def_navigation_bar_show);
+
             /*
              * IMPORTANT: Do not add any more upgrade steps here as the global,
              * secure, and system settings are no longer stored in a database
